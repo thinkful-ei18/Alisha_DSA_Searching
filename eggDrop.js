a9e843e96be3704c0bd5584e0a5612edc2f5fc26
alishaevn <alieva.inc@gmail.com>
2018-04-11 15:20:49 -0700

    created

---
+++ eggDrop.js
@@ -0,0 +1,4 @@
+'use strict';
+/*
+This is fun exercise to do - make this an optional one after you are done with all the exercises above Imagine that you wanted to find what the highest floor of a 100 story building you could drop an egg was, without the egg breaking. But you only have two eggs. Write an algorithm to work out which floors you should drop the eggs from to find this out in the most efficient way. Once you have understood the question and made some attempts to solve the problem, here is some reading to do before you start coding. http://datagenetics.com/blog/july22012/index.html
+*/
